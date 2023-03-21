b988aa9dd302e67e8b4ec36bce20ab14af441c27
Sam Black <sam.black@lubbockisd.org>
2023-03-21 08:52:10 -0500

    ArrayList basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayList<Integer> listOfNums = new ArrayList<>();
+        System.out.println(listOfNums);
+        listOfNums.add(27);
+        listOfNums.add(32);
+        listOfNums.add(-12);
+        listOfNums.add(1,512);
+        listOfNums.add(27);
+        listOfNums.remove(3);
+        listOfNums.remove(new Integer(27));
+        System.out.println(listOfNums.contains(27));
+        System.out.println(listOfNums);
+        System.out.println(listOfNums.set(1,973));
+        System.out.println(listOfNums.size());
+        Collections.sort(listOfNums);
+        System.out.println(listOfNums);
+        System.out.println(listOfNums.get(2));
+        for (int i = 0; i < listOfNums.size();i++){
+            listOfNums.set(i,listOfNums.get(i)+52);
+        }
+        for(int item:listOfNums){
+            item += 52;
+        }
+        System.out.println(listOfNums);
+
+
     }
 }
