485500a99ef1171f93778ec227bc99f2ef9497f3
Sam Black <sam.black@lubbockisd.org>
2023-03-28 09:02:57 -0500

    ArrayList of Dogs finished

--- src/DoggiesRunner.java
+++ src/DoggiesRunner.java
@@ -15,7 +15,17 @@ public class DoggiesRunner
 	
 	public static Dog findOldest( List<Dog> list )
 	{
-		return null;
+		Dog oldest = list.get(0);
+//		for(Dog d: list){
+//			if(d.getAge() > oldest.getAge())
+//				oldest = d;
+//		}
+//
+		for(int index = 1; index < list.size(); index++)
+			if(list.get(index).getAge() > oldest.getAge())
+				oldest = list.get(index);
+
+		return oldest;
 	}
 	
 	
@@ -28,22 +38,23 @@ public class DoggiesRunner
 		ray.add( new Dog( "ann", 21) );
 		
 		//add more Dogs to the list
-		
-		
-		
-		
+		ray.add(new Dog("alex",5));
+		ray.add(new Dog("mason", 3));
+		ray.add(new Dog("justin",4));
+
 		
 		System.out.println( ray );
 		
 		//write a for each loop to print out all of the Dogs
-		
-		
-		
-		
-		
-		
+//		for(Dog d: ray)
+//			out.println(d);
+
+		for(int i = 0; i < ray.size(); i++)
+			out.println(ray.get(i));
+
 		//call the method to find the oldest Dog
 		//print the oldest Dog
+		out.println(findOldest((ray)));
 		
 		System.out.println( "oldest dog == " + findOldest( ray ) );
 		
